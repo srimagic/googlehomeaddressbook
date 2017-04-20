e2ea672636f93029604230c052d1866dbda2be33
Sri <sridharv@gmail.com>
2017-04-20 14:57:55 -0500

    response

--- phonebook.js
+++ phonebook.js
@@ -1,5 +1,5 @@
 /*
-	desc: web api to support address book
+	desc: web hook api to support google actions api
 	author: srimagic
 	date: April 20th 2017
 	purpose: webhook for google actions api
@@ -8,6 +8,13 @@
 */
 (function(){
 	var PhoneBook = {},
+		response = {
+			"speech":"",
+			"displayText":"",
+			"data":{},
+			"contextOut":[],
+			"source":"Sri's phonebook"
+		},
 		getPerson = function(req){
 			console.log('req');
 			console.log(req.body);
@@ -20,44 +27,59 @@
 		add = function(req, res){
 
 			var person = getPerson(req);
+			response["speech"] = response["displayText"] = "";
 
 			if (person && person.name) {
 				PhoneBook[person.name] = person;
-				return res.sendStatus(201);
+				response["speech"] = response["displayText"] = "Sure. " + person.name + " has been added to your address book";
 			}
-			res.sendStatus(400);
+			response["speech"] = response["displayText"] = "sorry, bad parameters. please try again!!!";
+
+			return res.json(response);
 		},
 		remove = function(req, res){
 			var person = getPerson(req);
+			response["speech"] = response["displayText"] = "sorry, bad parameters. couldn't delete. please try again!!!";
 
 			if (person && person.name) {
 				delete PhoneBook[person.name];
-				return res.sendStatus(200);
+				response["speech"] = response["displayText"] = person.name + " has been removed";
 			}
-			res.sendStatus(400);
+
+			return res.json(response);
 		},
 		update = function(req, res){
 			var person = getPerson(req);
+			response["speech"] = response["displayText"] = "sorry, bad parameters. couldn't update. please try again!!!";
 
 			if (person && person.name) {
 				PhoneBook[person.name] = person;
-				return res.sendStatus(200);
+				response["speech"] = response["displayText"] = person.name + " has been updated";
+
 			}
-			res.sendStatus(400);
+			return res.json(response);
 		},
 		find = function(req, res){
 			var person = getPerson(req);
+			response["speech"] = response["displayText"] = "Sorry, no entry found with that name";
 
 			if (person && person.name) {
-				return res.json(PhoneBook[person.name]);
+				response["speech"] = response["displayText"] = JSON.stringify(PhoneBook[person.name]);
 			}
-			res.sendStatus(400);
+			return res.json(response);
 		},
 		findall = function(req, res){
-			res.json(PhoneBook);
+			response["speech"] = response["displayText"] = "Sorry, no entries found in your address book";
+
+			if (Object.keys(PhoneBook).length >0){
+				response["speech"] = response["displayText"] = JSON.stringify(PhoneBook);
+			}
+			
+			return res.json(response);
 		},
 		total = function(req, res){
-			res.json({'total':Object.keys(PhoneBook).length});
+			response["speech"] = response["displayText"] = "Your address book has a total of " + Object.keys(PhoneBook).length + " names";
+			return res.json(response);
 		},
 		debug = function(){
 			for (var name in PhoneBook) {
