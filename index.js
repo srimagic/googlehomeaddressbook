9660232925043c63f773c255f8c00e6ca3a2064b
Sri <sridharv@gmail.com>
2017-04-20 12:16:38 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    phoneBook = require('./phonebook'),
+    port = 6400
+;
+
+//add necessary middleware
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+//routes - general
+app.get('/', function(req, res){
+    res.send('Welcome to phonebook api!!!');
+});
+
+//routes - phone book
+app.post('/phonebook/add', phoneBook.add);
+app.post('/phonebook/del', phoneBook.del);
+app.post('/phonebook/update', phoneBook.update);
+app.post('/phonebook/find', phoneBook.find);
+app.post('/phonebook/findall', phoneBook.findall);
+app.post('/phonebook/total', phoneBook.total);
+
+
+//start listening for requests
+app.listen(port, function () {
+  console.log('phonebook listening on port ' + port);
+});
+
---
+++ phonebook.js
@@ -0,0 +1,69 @@
+/*
+	desc: web api to support address book
+	author: srimagic
+	date: April 20th 2017
+	purpose: webhook for google actions api
+	todos - 
+		duplicate names logic
+*/
+(function(){
+	var PhoneBook = {},
+		add = function(req, res){
+
+			var person = req.body;
+
+			if (person && person.name) {
+				PhoneBook[person.name] = person;
+				return res.sendStatus(201);
+			}
+			res.sendStatus(400);
+		},
+		remove = function(req, res){
+			var person = req.body;
+
+			if (person && person.name) {
+				delete PhoneBook[person.name];
+				return res.sendStatus(200);
+			}
+			res.sendStatus(400);
+		},
+		update = function(req, res){
+			var person = req.body;
+
+			if (person && person.name) {
+				PhoneBook[person.name] = person;
+				return res.sendStatus(200);
+			}
+			res.sendStatus(400);
+		},
+		find = function(req, res){
+			var person = req.body;
+
+			if (person && person.name) {
+				return res.json(PhoneBook[person.name]);
+			}
+			res.sendStatus(400);
+		},
+		findall = function(req, res){
+			res.json(PhoneBook);
+		},
+		total = function(req, res){
+			res.json({'total':Object.keys(PhoneBook).length});
+		},
+		debug = function(){
+			for (var name in PhoneBook) {
+				console.log(name);
+				console.log(PhoneBook[name]);
+			}
+		}
+	;
+
+	//exposed api
+	exports.add = add;
+	exports.del = remove;
+	exports.update = update;
+	exports.find = find;
+	exports.findall = findall;
+	exports.total = total;
+	
+})();
